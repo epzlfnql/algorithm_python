7108e68ee20981a97b3ff8f20ea2caa69ce4576e
Taeyoung <71575844+epzlfnql@users.noreply.github.com>
2022-09-17 21:56:05 +0900

    DP_이론정리_2

---
+++ 다이나믹프로그래밍/네트워크선자르기(topdown).py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+def DFS(len):
+    if dy[len]>0:
+        return dy[len]
+    if len==1 or len ==2:
+        return len
+    else:
+        dy[len] = DFS(len-1) + DFS(len-2) # 메모이제이션
+        return dy[len]
+
+n = int(input())
+dy = [0]*(n+1)
+print(DFS(n))
